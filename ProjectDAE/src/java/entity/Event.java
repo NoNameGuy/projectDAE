98f3b135eab4d5fdc14fb85ef853e3a8a51ba087
ff2009 <franciscoofernandes2008@hotmail.com>
2015-11-03 11:24:47 +0000

    Event changes
    Created constructors, getters and setters

--- ProjectDAE/src/java/entity/Event.java
+++ ProjectDAE/src/java/entity/Event.java
@@ -5,6 +5,10 @@
  */
 package entity;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import javax.ejb.Stateful;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -17,6 +21,32 @@ import javax.persistence.Id;
 public class Event {
     @Id
     private int id;
+    
+    protected Date date;
+    protected String name;
+    protected String type;
+    protected String local;
+    protected List<Participant> participantIds;
+    protected long responsableId;
+   
+    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    public Event() {
+        this.participantIds = new ArrayList<Participant>();
+    }
+
+    public Event(int id, Date date, String name, String type, String local, long responsableId) {
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.type = type;
+        this.local = local;
+        this.participantIds = new ArrayList<Participant>();
+        this.responsableId = responsableId;
+    }
 
     public int getId() {
         return id;
@@ -26,6 +56,53 @@ public class Event {
         this.id = id;
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public List<Participant> getParticipantIds() {
+        return participantIds;
+    }
+
+    public void setParticipantIds(List<Participant> participantIds) {
+        this.participantIds = participantIds;
+    }
+
+    public long getResponsableId() {
+        return responsableId;
+    }
+
+    public void setResponsableId(long responsableId) {
+        this.responsableId = responsableId;
+    }
+    
+    
 }
