cee51f6e2fe6242dfef649919cafc1dfa6dbf360
NoNameGuy <paulojose.m.vieira@gmail.com>
2015-11-03 11:05:03 +0000

    Administrator
    AdministratorBean
    Administrator class

---
+++ ProjectDAE/src/java/ejb/AdministratorBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import entity.Administrator;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author paulovieira
+ */
+@Stateless
+public class AdministratorBean {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void createAdmin(int id, String password, String name, String email, String role) {
+
+        try {
+            
+            Administrator admin = new Administrator(id, password, name, email, role);
+            em.persist(em);
+
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+
+    }
+    
+    
+    public void updateAdmin(int id, String password, String name, String email, String role) {
+        try {
+            Administrator admin = em.find(Administrator.class, id);
+            if (admin == null) {
+                return;
+            }
+            admin.setPassword(password);
+            admin.setName(name);
+            admin.setEmail(email);
+            admin.setRole(role);
+            em.merge(admin);
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+    
+        public void removeAdmin(int id) {
+        try {
+            Administrator admin = em.find(Administrator.class, id);
+            em.remove(admin);
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+    
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+}
--- ProjectDAE/src/java/entity/Administrator.java
+++ ProjectDAE/src/java/entity/Administrator.java
@@ -5,9 +5,10 @@
  */
 package entity;
 
-import javax.ejb.Stateless;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  *
@@ -15,8 +16,33 @@ import javax.persistence.Id;
  */
 @Entity
 public class Administrator {
+
     @Id
     private int id;
+    
+    @NotNull
+    private String password;
+    
+    @NotNull
+    private String name;
+    
+    @NotNull
+    @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\." + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message = "{invalid.email}")
+    private String email;
+    
+    @NotNull
+    private String role;
+
+    public Administrator() {
+    }
+
+    public Administrator(int id, String password, String name, String email, String role) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
 
     public int getId() {
         return id;
@@ -26,6 +52,38 @@ public class Administrator {
         this.id = id;
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
 }
