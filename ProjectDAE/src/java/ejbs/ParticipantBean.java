686721df631416d5c0d0b64e498f2098333ffc41
ff2009 <franciscoofernandes2008@hotmail.com>
2015-11-05 20:06:53 +0000

    Renamed Package EJB to EJBS

--- ProjectDAE/src/java/ejbs/ParticipantBean.java
+++ ProjectDAE/src/java/ejbs/ParticipantBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejb;
+package ejbs;
 
 import java.io.Serializable;
 import javax.ejb.Stateless;
