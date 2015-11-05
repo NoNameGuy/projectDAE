d87ed6c0abb0e141195c686235c6f4ca390f373c
NoNameGuy <paulojose.m.vieira@gmail.com>
2015-11-05 21:37:36 +0000

    Revert "Participant / Responsible"
    This reverts commit 09ed20eb5dd51ebc9148bdc48639e0d9a4709c77.

--- ProjectDAE/src/java/ejbs/ParticipantBean.java
+++ ProjectDAE/src/java/ejbs/ParticipantBean.java
@@ -5,9 +5,7 @@
  */
 package ejbs;
 
-import entity.Participant;
 import java.io.Serializable;
-import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,61 +18,7 @@ import javax.persistence.PersistenceContext;
 public class ParticipantBean implements Serializable {
     
     @PersistenceContext
-    private EntityManager em;
-    
-    //Create Participant
-
-    public void createParticipant(int id, String password, String name, String email) {
-
-        try {
-
-            Participant participant = new Participant(id, password, name, email);
-            em.persist(em);
-
-        } catch (Exception e) {
-            throw new EJBException(e.getMessage());
-        }
-
-    }
-
-    //Participant Update
-    
-    public void updateParticipant(int id, String password, String name, String email, String role) {
-        try {
-            Participant participant = em.find(Participant.class, id);
-            if (participant == null) {
-                return;
-            }
-            participant.setPassword(password);
-            participant.setName(name);
-            participant.setEmail(email);
-            //participant.setRole(role);
-            em.merge(participant);
-        } catch (Exception e) {
-            throw new EJBException(e.getMessage());
-        }
-    }
-
-    //remove Participant
-    
-    public void removeParticipant(int id) {
-        try {
-            Participant participant = em.find(Participant.class, id);
-            em.remove(participant);
-        } catch (Exception e) {
-            throw new EJBException(e.getMessage());
-        }
-    }
-
-    //Verify if participant exists
-    
-    public boolean existeParticipant(String username) {
-        try {
-            return em.find(Participant.class, username) != null;
-        } catch (Exception e) {
-            throw new EJBException(e.getMessage());
-        }
-    }
+    private EntityManager entityManager;
     
     public void listEvents () {
         
